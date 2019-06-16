13c7e6be1243344e5b0bea906a5f69cb796fc1b8
Akash Chavda <36260957+skychavda@users.noreply.github.com>
2019-06-16 13:49:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
+
+const app = express();
+
+app.use(cors());
+
+sgMail.setApiKey('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');
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+})
+
+app.get('/send-email', (req, res) => {
+  const msg = {
+    to: 'webcubetech.contact@gmail.com',
+    from: req.query.sender,
+    subject: req.query.subject,
+    text: req.query.message,
+  };
+  sgMail.send(msg).then((msg)=>{
+    console.log(msg);
+  });
+});
+
+app.listen(8080, () => console.log('running on port 8080'));
